bc30af5171ba57a25a353313f841462377bd4a4d
LuciaPuppo897 <alerome897@gmail.com>
2023-12-21 01:04:30 -0800

    add task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,6 +3,20 @@
 task 0 api getter
 """
 import requests
-import sys
+from sys import argv
 
+if __name__ == '__main__':
+    """ """
 
+api_url = f'https://jsonplaceholder.typicode.com/'
+
+user_id = int(argv[1])
+user_data = requests.get(api_url + f'users/{user_id}').json()
+user_task = requests.get(api_url + f'users/{user_id}/todos').json()
+user_completed_task = [task for task in user_task if task['completed']]
+
+print(f'Employee{user_data["name"]} is done with', end='')
+print(f'task({len(user_completed_task)}/{len(user_task)}):')
+
+for tasks in user_completed_task:
+    print("\task" + tasks["title"])

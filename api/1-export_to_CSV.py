0edd62b8989601691f011fef3eaf8a2f9abcaa34
LuciaPuppo897 <alerome897@gmail.com>
2024-02-11 21:54:05 -0800

    add documentation?

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -10,18 +10,17 @@ if __name__ == '__main__':
         api function
     """
 
-    api_url = f'https://jsonplaceholder.typicode.com/'
+    api_url = 'https://jsonplaceholder.typicode.com/'
 
-    user_id = (argv[1])
+    user_id = argv[1]
     user_data = requests.get(api_url + f'users/{user_id}').json()
     task_todo = requests.get(api_url + f'users/{user_id}/todos').json()
 
-
-with open(f'{user_id}.cvs', 'w', newline='') as csv_file:
-    writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-    for task in task_todo:
-        writer.writerow([user_id,
-                        user_data['username'],
-                        task['completed'],
-                        task['title'],
-                         ])
+    with open(f'{user_id}.csv', 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        for task in task_todo:
+            writer.writerow([user_id,
+                            user_data['username'],
+                            task['completed'],
+                            task['title'],
+                            ])
